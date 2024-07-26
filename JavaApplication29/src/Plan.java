96fdd97bbc0d43a70254e6ca59bfa9e2efe28952
anisvalle23 <anavalle23@unitec.edu>
2024-07-26 13:41:23 -0600

    plan class

--- JavaApplication29/src/Plan.java
+++ JavaApplication29/src/Plan.java
@@ -1,12 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author anavalle
- */
-public class Plan {
+public abstract class Plan {
     
+    private String numeroTelefono;
+    private String nombreCliente;
+
+    
+    public Plan(String numeroTelefono, String nombreCliente) {
+        this.numeroTelefono = numeroTelefono;
+        this.nombreCliente = nombreCliente;
+    }
+
+    
+    public String getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    
+    public abstract double pagoMensual(int mins, int msgs);
+
+    
+    public void imprimir() {
+        System.out.println("Número de Teléfono: " + numeroTelefono);
+        System.out.println("Nombre del Cliente: " + nombreCliente);
+    }
 }
+
+
